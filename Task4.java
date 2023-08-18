807737adb45bb714782a7d79a1ef27a63c929a36
Chirag Bhardwaj <67141112+Chirag2123@users.noreply.github.com>
2023-08-18 17:39:07 +0530

    Add files via upload
    Task 4 of Java Internship at Codsoft which include:
    1.Create a class to represent the ATM machine.
    2. Design the user interface for the ATM, including options such as withdrawing, depositing, and
    checking the balance.
    3. Implement methods for each option, such as withdraw(amount), deposit(amount), and
    checkBalance().
    4. Create a class to represent the user's bank account, which stores the account balance.
    5. Connect the ATM class with the user's bank account class to access and modify the account
    balance.
    6. Validate user input to ensure it is within acceptable limits (e.g., sufficient balance for withdrawals).
    7. Display appropriate messages to the user based on their chosen options and the success or failure
    of their transactions.

---
+++ Task4.java
@@ -0,0 +1,112 @@
+//Task4 - ATM INTERFACE
+
+import java.util.Scanner;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+}
+
+class ATM {
+    private BankAccount bankAccount;
+
+    public ATM(BankAccount account) {
+        bankAccount = account;
+    }
+
+    public void displayOptions() {
+        
+        System.out.println("1. Withdraw");
+        System.out.println("2. Deposit");
+        System.out.println("3. Check Balance");
+        System.out.println("4. Exit");
+    }
+
+    public void performOption(int option) {
+        switch (option) {
+            case 1:
+                withdraw();
+                break;
+            case 2:
+                deposit();
+                break;
+            case 3:
+                checkBalance();
+                break;
+            case 4:
+                System.out.println("Thank you for using the ATM.");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+    }
+
+    public void withdraw() {
+        Scanner chirag = new Scanner(System.in);
+        System.out.print("Enter the withdraw amount: ");
+        double amount = chirag.nextDouble();
+        if (amount > 0) {
+            if (amount <= bankAccount.getBalance()) {
+                bankAccount.withdraw(amount);
+                System.out.println("Withdrawal successful");
+            } else {
+                System.out.println("Insufficient balance");
+            }
+        } else {
+            System.out.println("Invalid amount");
+        }
+    }
+
+    public void deposit() {
+        Scanner chirag = new Scanner(System.in);
+        System.out.print("Enter the deposit amount: ");
+        double amount = chirag.nextDouble();
+        if (amount > 0) {
+            bankAccount.deposit(amount);
+            System.out.println("Deposit successful");
+        } else {
+            System.out.println("Invalid amount");
+        }
+    }
+
+    public void checkBalance() {
+        System.out.println("Current balance: " + bankAccount.getBalance());
+    }
+}
+
+public class Task4 {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(100000.0);
+        ATM atm = new ATM(bankAccount);
+        Scanner chirag = new Scanner(System.in);
+        System.out.println("Welcome to the CODSOFT 4th Task : ATM interface");
+
+        while (true) {
+            atm.displayOptions();
+
+            System.out.print("Enter your choice: ");
+            int option = chirag.nextInt();
+            atm.performOption(option);
+        }
+    }
+}
